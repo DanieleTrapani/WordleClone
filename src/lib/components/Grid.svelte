d4f6873ef631ce12f5c108971eaed6b6700a5b85
Daniele Trapani <daniele.trapani96@gmail.com>
2023-10-14 21:04:57 +0200

    layout

--- src/lib/components/Grid.svelte
+++ src/lib/components/Grid.svelte
@@ -1,13 +1,16 @@
 <script>
-  import Cell from "../components/Cell.svelte";
   import { rows } from "../stores/store";
 </script>
 
 <div class="wordGrid flex flex-col">
   {#each $rows as row}
     <div class="flex gap-1">
-      {#each row as cell}
-        <Cell content={cell} />
+      {#each row as content}
+        <div class="parent flex justify-center">
+          <div class="child">
+            {content}
+          </div>
+        </div>
       {/each}
     </div>
   {/each}
@@ -34,5 +37,20 @@
     .wordGrid {
       padding: 15px 520px;
     }
+  }
+
+  .parent {
+    height: 0;
+    border-width: 1.1px;
+    border-color: #525252;
+    flex-basis: 20%;
+    padding-bottom: 20%;
+    border-radius: 5px;
+  }
+
+  .child {
+    text-align: center;
+    font-size: 2rem;
+    color: rgb(210, 209, 209);
   }
 </style>
